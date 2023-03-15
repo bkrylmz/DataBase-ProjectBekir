b52a469c0913bd19e24001f2d3c37bdc5731eaaf
bekir <bkrylmz981@gmail.com>
2023-03-15 10:01:17 +0300

    changes

--- Day3_SQL/1.CreateTable_InsertInto_Update_Delete.sql
+++ Day3_SQL/1.CreateTable_InsertInto_Update_Delete.sql
@@ -29,3 +29,29 @@ VALUES (1,'Severus','Snape','Tester');
 INSERT INTO ScrumTeam VALUES (2,'Harold','Finch','Developer');
 INSERT INTO ScrumTeam VALUES (3,'Phoebe','Buffay','ScrumMaster');
 INSERT INTO ScrumTeam VALUES (4,'Michael','Scofield','PO');
+
+--UPDATE
+--how to update data
+/*
+UPDATE table_name
+SET column1 = value1,
+column2 = value2 , ...
+WHERE condition;
+*/
+UPDATE ScrumTeam
+SET JOBTITLE= 'SeniorTester',
+    LASTNAME= 'Yılmaz',
+    FIRSTNAME='Bekir'
+WHERE EMP_ID=4;
+
+--DELETE
+--Delete from table
+/*
+DELETE FROM table_name
+WHERE condition;
+*/
+DELETE FROM ScrumTeam
+WHERE EMP_ID=3; -- TÜM SATIRI SİLER ID NİN 3 OLDUĞU
+
+
+
